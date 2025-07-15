da30f7c903c3a0a7ba3fc09169da0c2bc53124b6
Yensi-Rakesh <rakesh@aiyensi.com>
2025-07-15 14:44:48 +0530

    Deleted README.md

--- api/Database/mongoData.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-from constants import mongoUrl,mongoDatabase,mongoMatchesCollection,mongoPlayersCollection,mongoscoreboardsCollection,mongoballsCollection
-client = MongoClient(mongoUrl)
-db = client[mongoDatabase]
-matchesCollection = db[mongoMatchesCollection]
-playersCollection = db[mongoPlayersCollection]
-scoreboardsCollection = db[mongoscoreboardsCollection]
-ballsCollection = db[mongoballsCollection]
